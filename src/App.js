ea90421cc7db8e187a5f7caa3fb3e2f245498c18
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-07 08:39:37 +0900

    🎉 my-weekly-rate 시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper className="App">
+      
+    </AppWrapper>
   );
 }
+
+const AppWrapper = styled.div`
+  max-width: 500px;
+  margin: auto;
+  background: #eee;
+`;
 
 export default App;
